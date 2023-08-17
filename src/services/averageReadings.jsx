07f8d27d44245fae8d72cf2aaece33512518bc4d
mauriciovzz <mauriciovr12@gmail.com>
2023-08-17 12:04:25 -0400

    Proxy config

--- src/services/averageReadings.jsx
+++ src/services/averageReadings.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/values";
+const baseUrl = "/api/values";
 
 const getAll = (node, variable) => {
   const request = axios.get(baseUrl);
--- src/services/locations.jsx
+++ src/services/locations.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/locations";
+const baseUrl = "/api/locations";
 
 const getLocation = (node) => {
   const request = axios.get(`${baseUrl}/${node.type}/${node.id}`);
