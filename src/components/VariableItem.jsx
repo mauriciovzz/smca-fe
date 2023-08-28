971f824df6278c26255b6919417d0889789fcfa3
mauriciovzz <mauriciovr12@gmail.com>
2023-08-27 22:32:36 -0400

    Database support for VaribleIrem

--- src/components/VariableItem.jsx
+++ src/components/VariableItem.jsx
@@ -2,21 +2,35 @@ import { React, useState, useEffect } from 'react';
 import averageReadingService from '../services/averageReadings';
 
 const VariableItem = ({ nodeLocation, variable }) => {
-  const [averageReadings, setAverageReadings] = useState([]);
+  const [averageReadings, setAverageReadings] = useState([{}]);
   const currentDate = new Date();
   const fullDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
-
-  useEffect(() => {
-    averageReadingService
-      .getAll(nodeLocation, variable, fullDate)
-      .then((requestedReadings) =>
-        setAverageReadings(requestedReadings));
-  }, []);
 
   const valueColor = (value) =>
     (
       value > 15 ? 'bg-lime-400' : 'bg-red-600'
     );
+
+  useEffect(() => {
+    averageReadingService
+      .getAll(nodeLocation, variable, fullDate)
+      .then((requestedReadings) => {
+        const objArray = [];
+
+        for (let i = 0; i < 24; i += 1) {
+          const readingMatch = requestedReadings.find((reading) =>
+            reading.end_hour === `${i}:00`);
+
+          objArray.push({
+            startTime: `${i}:00`,
+            endTime: `${i + 1}:00`,
+            value: (readingMatch) ? readingMatch.average_reading : null,
+            color: (readingMatch) ? valueColor(readingMatch.average_reading) : 'bg-white',
+          });
+        }
+        setAverageReadings(objArray);
+      });
+  }, []);
 
   return (
     <li
@@ -29,10 +43,8 @@ const VariableItem = ({ nodeLocation, variable }) => {
         </div>
 
         <div className="flex w-full">
-          {averageReadings.map((value) => {
-            const color = valueColor(value);
-            return <div key={value} className={`w-full flex-1 ${color}`} />;
-          })}
+          {averageReadings.map((reading) =>
+            <div key={reading.startTime} className={`w-full flex-1 ${reading.color}`} />)}
         </div>
       </div>
     </li>
